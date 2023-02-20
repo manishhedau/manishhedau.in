6d137d5d1de4632963d5584c731a19c449c79699
Manish Hedau <hedaumanish@gmail.com>
2023-02-20 20:36:45 +0530

    project setup

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+// import { Inter } from "@next/font/google";
+// import styles from '@/styles/Home.module.css'
+
+// const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Manish Hedau | Personal Portfolio</title>
+        <meta name="description" content="A personal portfolio site" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className="text-3xl font-bold underline">My personal blog</h1>
+      </main>
+    </>
+  );
+}
