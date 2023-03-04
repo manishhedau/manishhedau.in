80ee128cf5fb9cf2f99f3abcfa3c10af77ab236f
Manish Hedau <hedaumanish@gmail.com>
2023-03-04 13:43:09 +0530

    setup CI/CD pipeline with aws

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Manish Hedau | Personal Portfolio</title>
+        <title>Mr. Manish Hedau | Personal Portfolio</title>
         <meta name="description" content="A personal portfolio site" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
